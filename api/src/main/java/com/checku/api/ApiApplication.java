9fab456a5fa4bb452ecc39f2751433fbd47a8e7f
bazzi <0409gb@gmail.com>
2023-10-02 20:43:49 +0900

    [FEAT] User 생성 API 추가

--- api/src/main/java/com/checku/api/ApiApplication.java
+++ api/src/main/java/com/checku/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.checku.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan("com.checku.core")
+@EnableJpaRepositories("com.checku.core")
+@SpringBootApplication(scanBasePackages = {"com.checku.api", "com.checku.core"})
 public class ApiApplication {
 
     public static void main(String[] args) {
